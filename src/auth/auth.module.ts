ca77ea91e3f96b7f57de713add440009b0ad281a
d-kens <omondikens1@gmail.com>
2024-04-15 06:17:45 +0300

    setup jwt and passwort strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';;
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
+    PassportModule.register({
+      defaultStrategy: 'jwt'
+    }),
+    JwtModule.register({
+      secret: 'oiuytrewrghjkmnbvcxsw234567890876rewdsfghbvcxzcghew2134567890000000-=-0987trdfghjbvcxswe',
+      signOptions: {
+        expiresIn: 3600,
+      }
+    }),
     UsersModule
   ],
   controllers: [AuthController],
